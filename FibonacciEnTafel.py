40e86f37b63c9fc8c06f7021926489a2bb6c9cd6
abduzziwa <143699732+abduzziwa@users.noreply.github.com>
2024-09-23 16:06:04 +0200

    Update FibonacciEnTafel.py
    .

--- FibonacciEnTafel.py
+++ FibonacciEnTafel.py
@@ -20,11 +20,9 @@ def multiplication_table(n):
         return
 
     print("Multiplication Table")
-    # Print header row
     print("Tafel" + "\t" + "\t".join(str(i) for i in range(1, n + 1)))
 
     for i in range(1, n + 1):
-        # Print each row
         row = [str(i * j) for j in range(1, n + 1)]
         print(f"{i}\t" + "\t".join(row))
 
