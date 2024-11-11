ac2b3a9b23039d77c02996e70bcdbae98dd49b45
Abdul G Zziwa <abdul.zziwa@student.gildeopleidingen.nl>
2024-11-11 08:53:58 +0100

    New Changes.

---
+++ PHP/youtube-demo-Laravel/resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Registration Form</h2>
+
+    <form action="#" method="post" style="border: 1px solid #000; padding: 20px; width: 300px;">
+        <!-- Name Field -->
+        <input type="text" id="name" name="name" placeholder="name" required>
+
+        <!-- Email Field -->
+        <input type="email" id="email" name="email" required>
+
+        <!-- Password Field -->
+        <input type="password" id="password" name="password" required>
+
+        <!-- Submit Button -->
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
---
+++ c#/DataStructures/DataStructures/Program.cs
@@ -0,0 +1,79 @@
+﻿//Logging all values in the array 
+
+using System.Reflection.Metadata.Ecma335;
+
+string[] names = {"Peter","Jim","Suzzie","Dan"};
+
+for( int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+Console.WriteLine("....................................................");
+Console.WriteLine("numbers");
+
+int[] ages = { 20, 26, 50, 100 };
+int total = 0;
+for( int i = 0; i < ages.Length; i++)
+{
+    total += ages[i];
+}
+
+Console.WriteLine(total);
+
+Console.WriteLine(".....................................................");
+
+Console.WriteLine(".....................min-max...................");
+
+Console.WriteLine(ages.Max(x => x));
+Console.WriteLine(ages.Min(x => x));
+
+Console.WriteLine("...................reversing.................");
+
+string[] reversed = names.Reverse().ToArray();
+for (int i = 0; i < reversed.Length; i++)
+{
+    Console.WriteLine(reversed[i]);
+}
+
+Console.WriteLine("..............Average..................");
+
+int totalNumberOfItems = ages.Length;
+int tataladditionOfItems = 0;
+
+for (int i = 0;i < ages.Length ;i++)
+{
+   totalNumberOfItems += ages[i];
+}
+
+int average = totalNumberOfItems / totalNumberOfItems;
+
+Console.WriteLine(average);
+
+Console.WriteLine(".................CheckNames..............");
+
+Console.Write("What name are you searching for:  ");
+string searchName = Console.ReadLine();
+bool found = false;
+
+for (int i = 0; i < names.Length ; i++)
+{
+   if (names[i] == searchName)
+    {
+        found = true;
+    }
+}
+
+if (found)
+{
+    Console.WriteLine("Name found........................");
+} 
+else
+{
+    Console.WriteLine("Not found");
+}
+
+
+
+
+
